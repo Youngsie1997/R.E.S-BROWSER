8c8fb85c1b466bf922d29667563308fc2083723d
Youngsie1997 <Youngsie1997@gmail.com>
2016-02-05 14:47:04 +0000

    Added a bunch of shows
    Signed-off-by: Youngsie1997 <Youngsie1997@gmail.com>

--- popup.js
+++ popup.js
@@ -13,11 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var tvEpisode = 1;
 
 
-    if(tvShow == "American Dad") //American Dad
+    if(tvShow == "american_dad") //American Dad
     {
-      tvShow = "american_dad"
       tvSeason = rand(1,12);
-
       switch(tvSeason)
       {
         case 1:
@@ -35,11 +33,29 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    else if(tvShow == "Bob's Burgers") //Bob's Burgers
+    else if(tvShow == "baby_daddy")
     {
-      tvShow = "bobs_burgers";
-      tvSeason = rand(1,5);
+      tvSeason = rand(1,4);
+      switch(tvSeason)
+      {
+        case 1:
+          tvEpisode = rand(1,10);
+          break;
+        case 2:
+          tvEpisode = rand(1,19);
+          break;
+        case 3:
+          tvEpisode = rand(1,21);
+          break;
+        case 4:
+          tvEpisode = rand(1,22);
+          break;
+      }
+    }
 
+    else if(tvShow == "bobs_burgers") //Bob's Burgers
+    {
+      tvSeason = rand(1,5);
       switch(tvSeason) //Tv Season
       {
         case 1:
@@ -57,11 +73,9 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    else if(tvShow == "Brooklyn Nine-Nine") //Brooklyn Nine-Nine
+    else if(tvShow == "Brooklyn_Nine-Nine") //Brooklyn Nine-Nine
     {
-      tvShow = "Brooklyn_Nine-Nine";
       tvSeason = rand(1,3);
-
       switch(tvSeason)
       {
         case 1:
@@ -76,11 +90,9 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    else if(tvShow == "Community")
+    else if(tvShow == "community") //Community
     {
-      tvShow = "community";
-      tvseason = rand(1,6);
-
+      tvSeason = rand(1,6);
       switch(tvSeason)
       {
         case 1:
@@ -98,12 +110,46 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    else if(tvshow == "New Girl")
+    else if(tvShow == "family_guy") //Family Guy
     {
-      tvshow = "new_girl";
-      tvSeason = rand(1,4);
+      tvSeason = rand(1,14);
+      switch(tvSeason)
+      {
+        case 1:
+          tvEpisode = rand(1,7);
+          break;
+        case 2: case 8: case 12:
+          tvEpisode = rand(1,21);
+          break;
+        case 3: case 11:
+          tvEpisode = rand(1,22);
+          break;
+        case 4:
+          tvEpisode = rand(1,30);
+          break;
+        case 5: case 9: case 13:
+          tvEpisode = rand(1,18);
+          break;
+        case 6:
+          tvEpisode = rand(1,12);
+          break;
+        case 7:
+          tvEpisode = rand(1,16);
+          break;
+        case 10:
+          tvEpisode = rand(1,23);
+          break;
+        case 14:
+          tvEpisode = rand(1,12);
+          break;
+      }
 
-      switch(tvshow)
+    }
+
+    else if(tvShow == "new_girl") //New Girl
+    {
+      tvSeason = rand(1,4);
+      switch(tvSeason)
       {
         case 1:
           tvEpisode = rand(1,24);
@@ -115,12 +161,53 @@ document.addEventListener('DOMContentLoaded', function() {
           tvEpisode = rand(1,23);
           break;
         case 4:
-
-
+          tvEpisode = rand(1,22);
+          break;
       }
     }
 
+    else if(tvShow == "rick_and_morty") //Rick And Morty
+    {
+      tvSeason = rand(1,2);
+      switch(tvSeason)
+      {
+        case 1:
+          tvEpisode = rand(1,11);
+          break;
+        case 2:
+          tvEpisode = rand(1,10);
+          break;
+      }
+    }
 
+    else if(tvShow == "south_park")
+    {
+      tvSeason = rand(1,19);
+      switch(tvSeason)
+      {
+        case 1:
+          tvEpisode = rand(1,13);
+          break;
+        case 2:
+          tvEpisode = rand(1,18);
+          break;
+        case 3: case 4: case 6:
+          tvEpisode = rand(1,17);
+          break;
+        case 5: case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: case 16:
+          tvEpisode = rand(1,14);
+          break;
+        case 7:
+          tvEpisode = rand(1,15);
+          break;
+        case 17: case 18: case 19:
+          tvEpisode = rand(1,10);
+          break;
+      }
+    }
+
+    else if(tvShow == "the_simpsons")
+    {}
     window.open("http://thewatchseries.to/episode/"+tvShow+"_s"+tvSeason+"_e"+tvEpisode+".html")
   })
 })
